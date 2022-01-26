09816b1dc0e7d6f5775dd96ffa38cdcd3a323c7e
Ilya <ilyag87@mail.ru>
2022-01-26 23:55:32 +0300

    Initial project version

---
+++ mvc/src/test/java/com/springproj/mvc/ProjmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproj.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
