0f51032c9cec4d389a0789e8fa92b90cc1e31102
iluser <ilyag87@mail.ru>
2022-01-30 22:52:06 +0300

    Initial priject

---
+++ mvc/src/main/java/Controler/Controller.java
@@ -0,0 +1,36 @@
+package com.example.servingwebcontent;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "/welcome";
+
+    }
+
+//    @GetMapping("/listUser")
+//    public String greeting(@RequestParam(name="name", required=false) String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "listUser";
+//    }
+
+//    @PutMapping("/welcome")
+//    public String greeting(@RequestParam(name="name", required=false) String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "welcome";
+//    }
+
+//    @DeleteMapping
+//    public String greeting(@RequestParam(name="name", required=false) String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "none";
+//    }
+}
