47663888a0aa0ae6dd017167a7a300211eab7561
iluser <ilyag87@mail.ru>
2022-02-08 23:48:32 +0300

    add UsersTest of  Data Message

---
+++ mvc/src/test/Data/Message/UsersTest.java
@@ -0,0 +1,32 @@
+package Data.Message;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
